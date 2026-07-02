5c693d2d98c7254dd2d8e96f7c42bd3eee56ea37
shayyan2455-blip <shayyan2455@gmail.com>
2026-07-02 12:46:14 +0500

    Remove failed Vercel routing attempts

--- backend/api/v1/[...path].js
+++
@@ -1,9 +0,0 @@
-const app = require('../../src/app');
-
-module.exports = (req, res) => {
-  // Log the incoming request for debugging
-  console.log('Incoming request:', req.url, req.method);
-  
-  // Handle the request with Express
-  app(req, res);
-};
