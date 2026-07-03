a8b7b3f6bb5e0277ef78c4f14b5fe3bc8cc83cbd
shayyan2455-blip <shayyan2455@gmail.com>
2026-07-03 18:14:01 +0500

    Fix Vercel serverless handler for async app creation
    - Make handler async to await app creation
    - Cache app instance to avoid recreating on each request
    - Fixes 'app is not a function' error in serverless environment

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
-const app = require('../backend/src/app');
+const createApp = require('../backend/src/app');
+let appInstance = null;
 
-module.exports = (req, res) => {
+async function getApp() {
+  if (!appInstance) {
+    appInstance = await createApp();
+  }
+  return appInstance;
+}
+
+module.exports = async (req, res) => {
   if (!req.url.startsWith('/api')) {
     req.url = `/api${req.url === '/' ? '' : req.url}`;
   }
 
+  const app = await getApp();
   return app(req, res);
 };
