f5970a8955550aa0bde1a2c8ebe5f9e842acfafc
shayyan2455-blip <shayyan2455@gmail.com>
2026-07-08 15:59:48 +0500

    Fix expensePaymentApi to use ES6 module syntax
    - Change from CommonJS (require/module.exports) to ES6 (import/export)
    - Matches the pattern used by other API files in the frontend
    - Fixes Vercel build error

--- frontend/src/api/expensePaymentApi.js
+++ frontend/src/api/expensePaymentApi.js
@@ -1,10 +1,8 @@
-const api = require('./api');
+import api from './axiosClient';
 
-const expensePaymentApi = {
+export const expensePaymentApi = {
   list: (expenseId) => api.get(`/expense-payments/${expenseId}/payments`),
   create: (expenseId, data) => api.post(`/expense-payments/${expenseId}/payments`, data),
   update: (expenseId, id, data) => api.put(`/expense-payments/${expenseId}/payments/${id}`, data),
   remove: (expenseId, id) => api.delete(`/expense-payments/${expenseId}/payments/${id}`)
 };
-
-module.exports = expensePaymentApi;
